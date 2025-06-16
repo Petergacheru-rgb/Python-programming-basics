848f42a83e92efa7092fb3cc1e762e3d4193b88d
petergacheru-rgb <petergacheru44@gmail.com>
2025-06-16 20:50:01 +0300

    initial commit

---
+++ Datatypes.py
@@ -0,0 +1,26 @@
+number = 45 #Integer
+second = 34.98 #float
+greeting = "Good Morning" #String
+isPythonInnteresting =True #Boolean
+
+#Data Structures
+fruits = ["apple","banana","orange"] # List
+cars = ("BMW","Bently","Subaru") # Tuple
+countries = {"Kenya","Japan","Argentina"} #Set
+student = {
+    "Firstname":"Peter",
+    "Lastname" :"Gacheru",
+    "age" : 19,
+    "Nationality" : "Kenyan",
+} # Dictionary-Key-value pair
+print(number)
+print(isPythonInnteresting)
+print(fruits)
+print(cars)
+print(countries)
+print(student)
+print(student["Nationality"])
+
+#Typecasting-Converting one datatype to another
+print(float (number))
+print(int(second))
---
+++ Introduction.py
@@ -0,0 +1,5 @@
+#print is a method used to output a value
+print("Welcome to python programming language")
+print("PETER GACHERU")
+print(100)
+print(10+20)
